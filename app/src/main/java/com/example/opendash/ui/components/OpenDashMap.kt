3f9c91c1856e2a615e002c22c19b3dccae4c5c3d
Himjyoti Thakuria <92359223+H1mJT@users.noreply.github.com>
2026-09-08 23:45:55 +0530

    Import Compose state collector for map provider

--- app/src/main/java/com/example/opendash/ui/components/OpenDashMap.kt
+++ app/src/main/java/com/example/opendash/ui/components/OpenDashMap.kt
@@ -8,6 +8,7 @@ import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.graphics.Path
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
